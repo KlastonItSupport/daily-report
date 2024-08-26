f6281857993853b71b5894a123ce9f9f4e5d2c48
KlastonItSupport <it.support@klaston.com>
2024-08-26 15:05:50 -0300

    feat: updating api url

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://daily-report-api-production.up.railway.app/",
+  baseURL: "https://93.127.210.174:3001/",
 });
+// "http://localhost:3000/",
